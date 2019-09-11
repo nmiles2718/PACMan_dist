43418b445dec0bf500cabe03619212c5fd41f6c3
Nathan Miles <nmiles@osx-summer15.stsci.edu>
2019-09-11 14:39:08 -0400

    fixed some issues with scraping, small adjustments to main pacman module, playing around with test datasets in the notebooks to evaluate the performance of the classifier workflow

--- pacman2020.py
+++ pacman2020.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+import glob
 import logging
 import os
+import re
 
 
 import matplotlib.pyplot as plt
@@ -10,7 +12,10 @@ import numpy as np
 import pandas as pd
 import spacy
 from spacy.lang.en import English
+from spacy.tokenizer import Tokenizer
+# from spacy.util import compile_prefix_regex, compile_infix_regex, compile_suffix_regex
 import string
+import tqdm
 
 
 logging.basicConfig(format='%(levelname)-4s '
@@ -45,7 +50,7 @@ def read_stop_words(
 
     # Compare the stop words to the defaults used in spaCy
     missing_stop_words = stop_words.difference(default_stop_words)
-
+    
     if len(missing_stop_words) !=0:
         # These are both sets, so we take the union (i.e. we 'or' them)
         default_stop_words |= set(missing_stop_words)
@@ -53,7 +58,14 @@ def read_stop_words(
     return default_stop_words
 
 
-def spacy_tokenizer(text, stop_words=[], punctuations=[]):
+def read_category_label(fname):
+
+    flabel = fname.replace('Scientific_Justification','Scientific_Category')
+    with open(flabel, 'r') as fobj:
+        label = fobj.readlines()[0].strip().strip('\n')
+    return label
+
+def spacy_tokenizer(text, nlp=None, stop_words=[], punctuations=[]):
     """ Tokenizer using the spaCy nlp toolkit.
 
     Parameters
@@ -73,14 +85,20 @@ def spacy_tokenizer(text, stop_words=[], punctuations=[]):
 
     """
     # Creating tokenize the input next
-    parser = English()
-    mytokens = parser(text)
+    if nlp is None:
+        parser = English()
+        doc = parser(text)
+    else:
+        doc = nlp(text)
+    
+    mytokens = [token for token in doc]
     num_tokens = len(mytokens)
 
+    
 # TODO: look into potential lemmatization issues with similar words used in different manners (e.g. galaxy and galactic)
-    # Next, lemmatize each token and standardize the capitalization to be lower case
+    # Next, get the lemma of each token and force it to be lower case
     mytokens = [
-        word.lemma_.lower().strip()
+        word.lemma_.lower().strip('')
         if word.lemma_ != "-PRON-" else word.lower_
         for word in mytokens
     ]
@@ -90,6 +108,11 @@ def spacy_tokenizer(text, stop_words=[], punctuations=[]):
         word for word in mytokens
         if word not in stop_words and word not in punctuations
     ]
+    # Keep anything that is a letter,number, or has a -
+    # pattern = re.compile('[^a-zA-Z0-9-]')
+    pattern = re.compile('[^a-zA-Z-]')
+    mytokens = [word for word in mytokens if not pattern.match(word)]
+
     # print(f"Processed text represents "
     #       f"{100*len(mytokens) / num_tokens:0.2f}% of the input text")
 
@@ -97,9 +120,9 @@ def spacy_tokenizer(text, stop_words=[], punctuations=[]):
 
 
 def tokenize(
-        N=20,
-        fname='/Users/nmiles/PACMan_dist/notebooks/test_sci_justification.txt',
-
+    N=20,
+    fname='/Users/nmiles/PACMan_dist/notebooks/test_sci_justification.txt',
+    plot =False
 ):
     """ TODO: FINISH UP THIS PORTION
 
@@ -121,28 +144,75 @@ def tokenize(
         fname='/Users/nmiles/PACMan_dist/libs/stopwords.txt',
         default_stop_words=nlp.Defaults.stop_words
     )
+    
     # Reset the default list to contain all the words from spaCy stop word list
     # and our custom stop word list
     nlp.Defaults.stop_words = stop_words
-
+    # nlp.tokenizer = custom_tokenizer(nlp)
+    
     # Read in the Sci. Justif. Section
     with open(fname, 'r') as test_file:
         text = test_file.readlines()
         text = [val.strip('\n') for val in text]
         text = ' '.join(text)
 
+
     # Tokenize the example text
+    # doc = nlp(text)
+    # tokens = [token.text for token in doc if not token.is_stop and not token.is_space]
     tokens = spacy_tokenizer(
         text,
+        nlp=nlp,
         stop_words=stop_words,
         punctuations=string.punctuation
     )
-    s = pd.Series(tokens)
-    distribution = s.value_counts()
-    ax=distribution[:N].plot.barh()
-    ax.set_title(os.path.basename(fname))
-    plt.show()
-    return text, tokens
+    cleaned_text = ' '.join(tokens)
+
+    if plot:
+        s = pd.Series(tokens)
+        distribution = s.value_counts()
+        fig, ax = plt.subplots(nrows=1, ncols=1)
+        distribution[:N].plot.barh(ax=ax)
+        ax.set_title(os.path.basename(fname))
+        ax.set_xlim(0, 25)
+        plt.show()
+
+    return text, cleaned_text, tokens
+
+
+def read_in_dataset(flist_label=None, flist_text=None, notebook=False):
+    """
+    Parameters
+    ----------
+    flist : None, optional
+        Description
+    """
+    if flist_label is None:
+        flist_label = glob.glob(
+            '/Users/nmiles/PACMan_dist/C25/sci_just/*Category.txt'
+        )
+
+    if flist_text is None:
+        flist_text = glob.glob(
+            '/Users/nmiles/PACMan_dist/C25/sci_just/*Justification.txt'
+        )
+
+    if notebook:
+        statusbar = tqdm.tqdm_notebook
+    else:
+        statusbar = tqdm.tqdm
+    data = {'text':[], 'category':[]}
+    LOG.info('Reading in dataset...')
+    for f1, f2 in statusbar(zip(flist_label, flist_text)):
+        label = read_category_label(f1)
+        data['category'].append(label)
+        text, cleaned_text, tokens = tokenize(fname=f2, plot=False)
+        data['text'].append(text)
+
+    df = pd.DataFrame(data)
+    return df, data
+
+
 
 
 if __name__ == '__main__':
--- scrape/proposal_scraper.py
+++ scrape/proposal_scraper.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import glob
 import logging
 import os
@@ -10,8 +11,18 @@ logging.basicConfig(format='%(levelname)-4s '
                            '[%(module)s.%(funcName)s:%(lineno)d]'
                            ' %(message)s')
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-v', '--verbose',
+    help="Be verbose", 
+    default="NOTSET",
+    action="store_const", 
+    dest="loglevel",
+     const=logging.INFO,
+)
+args = parser.parse_args()
 LOG = logging.getLogger('proposal_scraper')
-LOG.setLevel(logging.INFO)
+LOG.setLevel(level=args.loglevel)
 
 class ProposalScraper(object):
     """
@@ -20,15 +31,17 @@ class ProposalScraper(object):
 
     """
 
-    def __init__(self, fname):
-        self._fname = fname
-        self._keywords = {
+    def __init__(self):
+        self._fname = None
+        self._archival = False
+        self._proposal_label = {
             'Scientific Category': None,
             'Scientific Keywords': None,
         }
 
         self._section_data = {
             'Abstract': None,
+            'Investigators': None,
             'Scientific Justification': None,
             'Description of the Observations': None,
             'Special Requirements': None,
@@ -36,7 +49,24 @@ class ProposalScraper(object):
             'Analysis Plan': None
         }
 
+        self._section_data_archival = {
+            'Abstract': None,
+            'Investigators': None,
+            'Scientific Justification': None,
+            'Analysis Plan': None,
+            'Management Plan': None
+        }
+
         self._text = None
+
+    @property
+    def archival(self):
+        """Flag to specify if proposal is AR or not"""
+        return self._archival
+
+    @archival.setter
+    def archival(self, value):
+        self._archival = value
 
     @property
     def fname(self):
@@ -48,13 +78,13 @@ class ProposalScraper(object):
         self._fname = value
 
     @property
-    def keywords(self):
+    def proposal_label(self):
         """Keywords to save from the proposal template"""
-        return self._keywords
+        return self._proposal_label
 
-    @keywords.setter
-    def keywords(self, value):
-        self._keywords = value
+    @proposal_label.setter
+    def proposal_label(self, value):
+        self._proposal_label = value
 
     @property
     def section_data(self):
@@ -66,6 +96,15 @@ class ProposalScraper(object):
         self._section_data = value
 
     @property
+    def section_data_archival(self):
+        """Section Names defined in the Phase I template"""
+        return self._section_data_archival
+
+    @section_data_archival.setter
+    def section_data_archival(self, value):
+        self._section_data_archival = value
+
+    @property
     def text(self):
         return self._text
 
@@ -74,7 +113,7 @@ class ProposalScraper(object):
         self._text = value
 
     def read_file(self, fname=None):
-        """ Read the file
+        """ Read the file and determine the proposal type
 
         Parameters
         ----------
@@ -85,69 +124,227 @@ class ProposalScraper(object):
         -------
 
         """
+        # Create regex pattern for removing punctuation while keeping
+        # remove = string.punctuation
+        # remove = remove.replace("-", "") # don't remove hyphens
+        # pattern = r"[{}]".format(remove) # create the pattern
         if fname is not None:
             self.fname = fname
+
         with open(self.fname, 'r') as fobj:
             lines = fobj.readlines()
             text = [line.strip('\n') for line in lines]
-            text = [sent for sent in text if sent]
+            text = [sentence for sentence in text if sentence]
             text = [
-                re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]', '', sent)
-                for sent in text
+                re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]', ' ', sentence)
+                for sentence in text
             ]
-
+            # text = [
+            #     re.sub(r'[^a-zA-Z0-9-]+', ' ', sentence)
+            #     for sentence in text
+            # ]
         self._text = text
+
+        if 'AR' in self.text[1]:
+            self.archival = True
+        else:
+            self.archival = False
 
     def extract_keywords(self):
         """Extract the keyword info above the abstract section"""
         i=0
         while self.text[i] != 'Abstract':
-            for key in self.keywords.keys():
+            for key in self.proposal_label.keys():
                 if key in self.text[i]:
-                    self.keywords[key] = self.text[i].split(':')[-1]
+                    self.proposal_label[key] = self.text[i].split(':')[-1]
             i += 1
 
     def extract_sections(self):
-        i = 0
-        section_start = None
-        section_end = None
-        section_names = list(self.section_data.keys())
-        while i < len(self.text):
-            for k, key in enumerate(section_names):
-                if k+1 == len(section_names):
-                    last = True
+        
+        # Set the line number counter
+        current_line_num = 0
+        
+        # Check to see if the proposal is archival or not and grab the correct section names
+        if self.archival:
+            section_names = list(self.section_data_archival.keys())
+            section_data = self.section_data_archival
+        else:
+            section_names = list(self.section_data.keys())
+            section_data = self.section_data
 
-                if key in self.text[i]:
-                    section_start = i
-                    j = i
-                    while j < len(self.text):
+        # Initialize the current idx for the section_names list
+        current_section_idx = 0
+        
+        # Initialize a variable for computing the index of the next section.
+        # This is required because proposals might be missing sections and so
+        # when look for all data between two sections we need to have fine control
+        # over what section is used to define the end of the current section
+        next_idx = 1
+        
+        # Start looping through the text from line 0
+        while current_line_num < len(self.text):
+            # The current section we are searching for
+            current_section = section_names[current_section_idx]
+            
+            # Line number corresponding to start and stop of current section
+            section_start = None
+            section_end = None
+            
+            # If we find our section name in the current line number, this begins the
+            # start of our section. 
+            if current_section in self.text[current_line_num]:
+                # Section headers have their own line and so the true start is the next index
+                section_start = current_line_num + 1 
+                LOG.info(f'{current_section} starts on line {section_start}')
+                
+                # Compute the index for the next section in the list 
+                next_section_idx = current_section_idx + next_idx
+                try:
+                    next_section = section_names[next_section_idx]
+                except IndexError:
+                    # If we've exhausted all section names, write the remaining portion of the file into the current section
+                    LOG.info('Exhausted all section names, writing remaining lines into current section')
+                    section_end = len(self.text)
+    #                 print(f'Extracting lines {section_start} to {section_end} for {current_section}')
+                    section_data[current_section] = self.text[section_start:section_end]
+                    break
+                
+                # If we haven't exhausted all section names, continue looping through lines
+                # until we've found the next section
+    #             print(f'Looking for text between {current_section} and {next_section}')
+                j = section_start
+                while j < len(self.text):
+                    text = self.text[j]
+                    # if the next section title is in the current text, we've found the end
+                    if next_section in text:
+                        section_end = j - 1
+                        LOG.info(f'{current_section} ends on line {section_end}')
+                        current_section_idx +=1
+                        # Set the current line number to the end of the section we just found
+                        # This ensure the loops picks up on the next section
+                        current_line_num = j - 1
+                        break
+                        
+                    # Increase j by one to step to the next line
+                    j += 1
+                    
+                    # If we hit the end of the file and we never found the section end
+                    # increase the next_idx by one and search for the next section
+                    if j >= len(self.text) and section_end is None :
+                        LOG.info(f'Reached the end of file without finding {next_section}')
+                        j = section_start
+                        next_idx +=1
+                        next_section_idx = current_section_idx + next_idx
                         try:
-                            sname = section_names[k+1]
-                        except IndexError:
-                            # Write out everything below the last element to
-                            # the Analysis Plan section
-                            sname = section_names[k]
-
-                        try:
-                            t = self.text[j]
-                        except IndexError:
-                            # Break the loop, no need to keep going if we've
-                            # made it through the test
+                            next_section = section_names[next_section_idx]
+                        except IndexError as e:
+                            # Exhausted the list again!
+                            section_end = len(self.text)
+                            current_line_num = section_end
                             break
+                        else:
+                            LOG.info(f'Restarting from line {section_start} and searching for text between {current_section} {section_names[next_section_idx]}')
 
-                        if sname in t:
-                            section_end = j
-                            i = j
-                            break
+                LOG.info(f'Extracting lines {section_start} to {section_end} for {current_section}')
+                section_data[current_section] = self.text[section_start:section_end]
 
-                        j+=1
+                current_section_idx = next_section_idx
+                next_idx = 1
+            current_line_num+=1
+        if self.archival:
+            self.section_data_archival = section_data
+        else:
+            self.section_data = section_data
 
-                    self.section_data[key] = self.text[
-                                             section_start:section_end
-                                             ]
-            i+=1
 
-    def write_data(self, section_name):
+    # def extract_sections(self):
+    #     i = 0
+    #     section_start = None
+    #     section_end = None
+    #     if self.archival:
+    #         section_names = list(self.section_data_archival.keys())
+    #         section_data = self.section_data_archival
+    #     else:
+    #         section_names = list(self.section_data.keys())
+    #         section_data = self.section_data
+
+    #     current_section_idx = 0
+    #     while i < len(self.text):
+    #         current_section = section_names[current_section_idx]
+    #         if current_section in self.text[i]:
+    #             section_start = i + 1
+    #             LOG.info(f'{current_section} starts on line {section_start}')
+    #             j = i
+    #             while j < len(self.text):
+    #                 try:
+    #                     next_section = section_names[current_section_idx+1]
+    #                 except IndexError:
+    #                     LOG.info('Exhausted all section names...')
+    #                     # If we've exhausted all section names, write the remaining portion of the file into the last section
+    #                     section_end = len(self.text)
+    #                     i = len(self.text)
+    #                     break
+
+    #                 try:
+    #                     t = self.text[j]
+    #                 except IndexError:
+    #                     # Break the loop, no need to keep going if we've
+    #                     # made it through the text
+    #                     break
+
+    #                 if next_section in t:
+    #                     section_end = j - 1
+    #                     LOG.info(f'{current_section} ends on line {section_end}')
+    #                     current_section_idx +=1
+    #                     i = j - 1
+    #                     break
+    #                 j+=1
+    #             msg = (
+    #                 f"Extracting lines {section_start} to "+
+    #                  f"{section_end} for {current_section}\n{'-'*79}"
+    #                 )
+    #             LOG.info(msg)
+    #             section_data[current_section] = self.text[section_start:section_end]
+    #         i+=1
+    #     if self.archival:
+    #         self.section_data_archival = section_data
+    #     else:
+    #         self.section_data = section_data
+
+
+    def write_training_data(self, training_sections):
+        """Write out the training data we will use for text classification
+        """
+        outdir = os.path.join(os.path.dirname(self.fname), 'training_corpus')
+        try:
+            os.mkdir(outdir)
+        except FileExistsError:
+            pass
+
+        fout = os.path.basename(self.fname.replace('.txtx','_training.txt'))
+        fout = os.path.join(outdir, fout)
+
+        data = []
+        for section in training_sections:
+            try:
+                if self.archival:
+                    text = self.section_data_archival[section]
+                else:
+                    text = self.section_data[section]
+                if isinstance(text, list):
+                    text = '\n'.join(text)
+            except KeyError:
+                LOG.info(f'Missing info for {section}')
+            else:
+                # print(section, data[:10])
+                data.append(text)
+
+        data = '\n'.join(data)
+        with open(fout, mode='w') as fobj:
+            fobj.write(data)
+
+
+    def write_section_data(self, section_name):
         """ Write out the data for the section specified by section_name
 
         Parameters
@@ -158,7 +355,7 @@ class ProposalScraper(object):
         -------
 
         """
-        outdir = os.path.join(os.path.dirname(self.fname), 'sci_just')
+        outdir = os.path.join(os.path.dirname(self.fname), 'training_corpus')
 
         try:
             os.mkdir(outdir)
@@ -174,7 +371,7 @@ class ProposalScraper(object):
         try:
             data = self.section_data[section_name]
         except KeyError:
-            data = self.keywords[section_name]
+            data = self.proposal_label[section_name]
 
         if data is None:
             LOG.info('\nOops! Nothing to write out.\n'
@@ -203,15 +400,21 @@ class ProposalScraper(object):
             flist = glob.glob('/Users/nmiles/PACMan_dist/C25/*txtx')
 
         for f in tqdm.tqdm(flist):
+            LOG.info(f)
             self.read_file(fname=f)
             self.extract_sections()
             self.extract_keywords()
-            self.write_data(section_name='Scientific Justification')
+            self.write_section_data(
+                section_name='Scientific Category'
+            )
+            self.write_training_data(
+                training_sections=['Abstract', 'Scientific Justification']
+            )
 
 
 def main():
-    fname = '/Users/nmiles/PACMan_dist/C25/0948.txtx'
-    prop = ProposalScraper(fname=fname)
+    # fname = '/Users/nmiles/PACMan_dist/C25/0948.txtx'
+    prop = ProposalScraper()
     prop.extract_flist()
 
 if __name__ == '__main__':
